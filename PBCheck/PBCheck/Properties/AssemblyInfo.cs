f92d9a91f8927be00c261fda50d4d5c881f1defd
Paolo Bernardi <paolo@bernardi.cloud>
2021-01-21 07:32:49 +0100

    Improved project and executable metadata

--- PBCheck/PBCheck/Properties/AssemblyInfo.cs
+++ PBCheck/PBCheck/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PBCheck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool to check for missing software, directories and executables in the PATH")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Paolo Bernardi - Software Engineer")]
 [assembly: AssemblyProduct("PBCheck")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Paolo Bernardi 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
